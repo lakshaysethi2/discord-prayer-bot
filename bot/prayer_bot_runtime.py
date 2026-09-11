bb23617d45bae058bf9b244549b4e232fa653b68
lakshaysethi2 <65022931+lakshaysethi2@users.noreply.github.com>
2026-09-11 23:02:12 +1200

    refactor: compose all PrayerBot runtime mixins (#26)

--- bot/prayer_bot_runtime.py
+++ bot/prayer_bot_runtime.py
@@ -1,7 +1,16 @@
 """PrayerBot runtime mixins."""
 from bot.prayer_bot_voice import PrayerBotVoiceMixin
+from bot.prayer_bot_tts import PrayerBotTtsMixin
+from bot.prayer_bot_playback import PrayerBotPlaybackMixin
 from bot.prayer_bot_commands import PrayerBotCommandsMixin
 from bot.prayer_bot_status import PrayerBotStatusMixin
 
-class PrayerBotRuntimeMixin(PrayerBotVoiceMixin, PrayerBotCommandsMixin, PrayerBotStatusMixin):
+
+class PrayerBotRuntimeMixin(
+    PrayerBotVoiceMixin,
+    PrayerBotTtsMixin,
+    PrayerBotPlaybackMixin,
+    PrayerBotCommandsMixin,
+    PrayerBotStatusMixin,
+):
     pass
