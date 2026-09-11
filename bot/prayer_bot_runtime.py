13ae1a6dcd9f4ed3d78c0f85cf0103e5c7469c27
lakshaysethi2 <65022931+lakshaysethi2@users.noreply.github.com>
2026-09-11 22:56:33 +1200

    refactor: include status mixin in runtime barrel (#26)

--- bot/prayer_bot_runtime.py
+++ bot/prayer_bot_runtime.py
@@ -1,6 +1,7 @@
 """PrayerBot runtime mixins."""
 from bot.prayer_bot_voice import PrayerBotVoiceMixin
 from bot.prayer_bot_commands import PrayerBotCommandsMixin
+from bot.prayer_bot_status import PrayerBotStatusMixin
 
-class PrayerBotRuntimeMixin(PrayerBotVoiceMixin, PrayerBotCommandsMixin):
+class PrayerBotRuntimeMixin(PrayerBotVoiceMixin, PrayerBotCommandsMixin, PrayerBotStatusMixin):
     pass
